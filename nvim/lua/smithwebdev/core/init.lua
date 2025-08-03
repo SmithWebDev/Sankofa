0aac9cf900c7495a044bbaf381f4dcac73367ea0
Darrick Smith <smithwebdd@gmail.com>
2025-08-02 21:51:58 -0400

    [REFACTOR] Updated Core init file, rearranged files to ensure plugins use/can access global functions

--- nvim/lua/smithwebdev/core/init.lua
+++ nvim/lua/smithwebdev/core/init.lua
@@ -1,5 +1,8 @@
 require "smithwebdev.core.keybinds"
 require "smithwebdev.core.settings"
 require "smithwebdev.core.autocmds"
-require "smithwebdev.core.lazy"
 require "smithwebdev.core.functions"
+require "smithwebdev.core.lazy"
+
+
+vim.cmd([[colorscheme delek]])
