04263231b3f6f77018f6884968ff3908a2bb2ea4
Darrick Smith <smithwebdd@gmail.com>
2025-11-11 20:35:41 -0500

    [PLUG] Added treesitter, no neck pain, and comfy line numbers

---
+++ nvim/lua/smithwebdev/plugins/functional/vim_surround.lua
@@ -0,0 +1,10 @@
+    vim.g.surround_35='<%# \r %>' -- #
+    vim.g.surround_45='<% \r %>' -- -
+    vim.g.surround_61='<%= \r %>' -- =
+    vim.g.surround_65='<% \r do %>\n<% end %>' -- A
+    vim.g.surround_97='<% \r %>\n<% end %>' -- a
+    --vim.g.surround_102='<%  %>\n\r<% end %>' -- f
+    --vim.g.surround_70='<%  do %>\n\r<% end %>' -- F
+    vim.g.surround_102='function() \r end' -- f
+    vim.g.surround_70='function()\n\r end' -- F
+
--- nvim/lua/smithwebdev/plugins/init.lua
+++ nvim/lua/smithwebdev/plugins/init.lua
@@ -3,7 +3,7 @@
 SankofaPlugins.add(
   "https://github.com/rebelot/kanagawa.nvim", {
     config = function()
-      require'smithwebdev.plugins.colorschemes.kanagawa'
+      require 'smithwebdev.plugins.colorschemes.kanagawa'
       -- vim.cmd[[colorscheme kanagawa]]
     end
   }
@@ -12,7 +12,7 @@ SankofaPlugins.add(
 SankofaPlugins.add(
   'https://github.com/pauchiner/pastelnight.nvim', {
     config = function()
-      require'smithwebdev.plugins.colorschemes.pastelnight'
+      require 'smithwebdev.plugins.colorschemes.pastelnight'
       -- vim.cmd[[colorscheme pastelnight]]
     end
   }
@@ -23,7 +23,7 @@ SankofaPlugins.add(
     lazy = false,
     priority = 1000,
     config = function()
-      require'smithwebdev.plugins.colorschemes.silkcircuit'
+      require 'smithwebdev.plugins.colorschemes.silkcircuit'
       -- vim.cmd[[colorscheme silkcircuit]]
     end
   }
@@ -32,9 +32,9 @@ SankofaPlugins.add(
 SankofaPlugins.add(
   'https://github.com/folke/tokyonight.nvim', {
     config = function()
-      require'smithwebdev.plugins.colorschemes.tokyonight'
+      require 'smithwebdev.plugins.colorschemes.tokyonight'
       -- vim.cmd[[colorscheme tokyonight]]
-      vim.cmd[[colorscheme tokyonight-night]]
+      -- vim.cmd [[colorscheme tokyonight-night]]
       -- vim.cmd[[colorscheme tokyonight-storm]]
       -- vim.cmd[[colorscheme tokyonight-day]]
       -- vim.cmd[[colorscheme tokyonight-moon ]]
@@ -43,9 +43,9 @@ SankofaPlugins.add(
 )
 
 SankofaPlugins.add(
-  'https://github.com/V4N1LLA-1CE/xcodedark.nvim', {
+  'https://github.com/ianklapouch/wildberries.nvim', {
     config = function()
-      require'smithwebdev.plugins.colorschemes.xcodedark'
+      vim.cmd[[colorscheme wildberries]]
     end
   }
 )
@@ -54,8 +54,52 @@ SankofaPlugins.add(
 --------------------------------------------------------------------------------
 SankofaPlugins.add(
   'https://github.com/mluders/comfy-line-numbers.nvim', {
-    config = function ()
-      require'smithwebdev.plugins.functional.comfy_line_numbers'
+    config = function()
+      require 'smithwebdev.plugins.functional.comfy_line_numbers'
+    end
+  }
+)
+
+SankofaPlugins.add(
+  'https://github.com/shortcuts/no-neck-pain.nvim', {
+    version = "*",
+    config = function()
+      require 'smithwebdev.plugins.functional.no_neck_pain'
+    end
+  }
+)
+
+SankofaPlugins.add(
+  'https://github.com/nvim-treesitter/nvim-treesitter', {
+    build = ":TSUpdate",
+    dependencies = SankofaPlugins.deps_from_urls({
+      'https://github.com/OXY2DEV/markview.nvim',
+      'https://github.com/nvim-treesitter/playground',
+      'https://github.com/nvim-treesitter/nvim-treesitter-context',
+      'https://github.com/RRethy/nvim-treesitter-endwise',
+      'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+      'https://github.com/RRethy/nvim-treesitter-textsubjects',
+    }),
+    config = function()
+      require 'smithwebdev.plugins.functional.treesitter'
+    end
+  }
+)
+
+SankofaPlugins.add(
+  'https://github.com/folke/which-key.nvim', {
+    config = function()
+      require 'smithwebdev.plugins.navigation.which_key'
+    end
+  }
+)
+
+--- Language Server Plugins
+--------------------------------------------------------------------------------
+SankofaPlugins.add(
+  "https://github.com/bngarren/checkmate.nvim", {
+    config = function()
+      -- require('smithwebdev.plugins.functional.checkmate')
     end
   }
 )
