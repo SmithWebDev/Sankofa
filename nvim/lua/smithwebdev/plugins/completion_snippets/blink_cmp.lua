382245afd8b5e9bfab0f801ed1113163e3b625c2
DB Smith <smithwebdd@gmail.com>
2025-11-20 22:07:32 -0500

    [PLUG] Updated blink.cmp file

--- nvim/lua/smithwebdev/plugins/completion_snippets/blink_cmp.lua
+++ nvim/lua/smithwebdev/plugins/completion_snippets/blink_cmp.lua
@@ -1,46 +1,52 @@
--- local blink = require'blink.cmp'
--- local choice_maps = require('smithwebdev.core.utils.blink_luasnip').luasnip_choice_mappings("<C-l>", "<C-h>")
---
--- blink.setup()
-print("Blink config loaded")
 local blink = require("blink.cmp")
 
 blink.setup({
   completion = {
-    keyword = {
-      range = "prefix",
-    },
-    accept = {
-      auto_brackets = {
-        enabled = true,
-      },
-    },
-    menu = {
-      auto_show = true,
+    accept = { auto_brackets = { enabled = true, }, },
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+    ghost_text = { enabled = true },
+    keyword = { range = "prefix", },
+    list = { selection = { preselect = false, auto_insert = true } },
+    menu = { auto_show = true,
       draw = {
+        components = {
+          kind_icon = {
+            text = function(ctx)
+              local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+              return kind_icon
+            end,
+            -- (optional) use highlights from mini.icons
+            highlight = function(ctx)
+              local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+              return hl
+            end,
+          },
+          kind = {
+            -- (optional) use highlights from mini.icons
+            highlight = function(ctx)
+              local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+              return hl
+            end,
+          }
+        },
         columns = {
           { "label", "label_description", gap = 1 },
-          { "kind_icon", "kind" }
-        },
-      },
-    },
-    trigger = {
-      show_on_keyword = true,
-      show_on_insert = true,
-      show_on_trigger_character = true,
-    },
+          { "kind_icon", "kind" } }, }, },
+    trigger = { show_on_keyword = true, show_on_insert = true, show_on_trigger_character = true, },
   },
-  snippets = {
-    preset = "luasnip",
+  snippets = { preset = "luasnip", },
+  sources = {
+    default = { 'lsp', 'path','snippets', 'buffer' },
   },
   keymap = {
     preset = "default",
-    ["<C-j>"] ={ 'select_next', 'fallback' },
-    ["<C-k>"] ={ 'select_prev', 'fallback' }
+    ["<C-j>"] ={ 'select_next', 'snippet_forward', 'fallback' },
+    ["<C-k>"] ={ 'select_prev', 'snippet_backward', 'fallback' },
+    ["<c-n>"] = {'scroll_documentation_down', 'scroll_signature_up'},
+    ["<c-p>"] = {'scroll_documentation_up', 'scroll_signature_down'},
   },
   cmdline = {
     keymap = { preset = 'inherit' },
     completion = { menu = { auto_show = true } },
   }
 })
-
